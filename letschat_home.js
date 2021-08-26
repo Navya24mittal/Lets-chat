98495d54755b3c45cb4fc4c86fceb90da03e1aed
Navya24mittal <83725616+Navya24mittal@users.noreply.github.com>
2021-08-26 17:54:19 -0400

    Add files via upload

---
+++ letschat_home.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDb8c3ningyrmN_FBdLyL_4ZnBWyRVZFWU",
+    authDomain: "kwitter-259a8.firebaseapp.com",
+    databaseURL: "https://kwitter-259a8-default-rtdb.firebaseio.com",
+    projectId: "kwitter-259a8",
+    storageBucket: "kwitter-259a8.appspot.com",
+    messagingSenderId: "1042206142959",
+    appId: "1:1042206142959:web:5c44f3e02bcd2e596e061a",
+    measurementId: "G-NRFEKE40FY"
+  };
+  firebase.initializeApp(firebaseConfig);
